2b398b6127f0bff65f2e68cbc61ede03986b5cd1
Jose Manuel <jmdcarq89@gmail.com>
2021-04-24 16:57:42 +0200

    cast

---
+++ AdvancedTypes.ts
@@ -0,0 +1,23 @@
+interface Bird {
+  fly(): void;
+  layEggs(): void;
+}
+
+interface Fish {
+  swim(): void;
+  layEggs(): void;
+}
+
+declare function getSmallPet(): Fish | Bird;
+
+let pet = getSmallPet();
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+if (isFish(pet)) {
+  pet.swim();
+} else {
+  pet.fly();
+}
