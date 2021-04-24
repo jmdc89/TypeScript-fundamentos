3329161866b87a4715b31e33330a39c4836fd56f
Jose Manuel <jmdcarq89@gmail.com>
2021-04-24 17:05:02 +0200

    extension

--- AdvancedTypes.ts
+++ AdvancedTypes.ts
@@ -21,3 +21,36 @@ if (isFish(pet)) {
 } else {
   pet.fly();
 }
+
+//Diferencia entre interface y type
+//La interface puede ser extendida, el type no
+
+interface Animal {
+  name: string;
+}
+
+//para extender una interface
+interface Tiger extends Animal {
+  gender: string;
+}
+
+const tiger: Tiger = {};
+
+type Animal2 = {
+  name: string;
+};
+
+//tambien podemos extender interface
+//esto no podemos hacerlo en type
+interface Animal {
+  size: number;
+}
+
+//para extender un type
+type Tiger2 = Animal2 & {
+  gender: string;
+};
+
+const tiger2: Tiger2 = {
+  name: "hola",
+};
